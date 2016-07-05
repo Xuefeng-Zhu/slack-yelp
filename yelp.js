65a049e47d2bb0d8933bc1573afd1aba5fc664b1
Xuefeng-Zhu <xzhu15@illinois.edu>
2016-07-05 02:37:39 -0500

    improve the result format

--- yelp.js
+++ yelp.js
@@ -4,23 +4,22 @@ var Yelp = require('yelp');
 var INVALID_QUERY = 'Invalid query! (i.g. food in Chicago)';
 var NO_RESULT_TEMPLATE = _.template('0 result found for your query <%= query %>');
 var RESULTS_TEMPLATE = _.template('<%= count %> results found for your query <%= query %>')
-var RESULT_TEMPLATE = _.template('<%= name %> at <%= location.display_address %>');
 
-var yelpConfig = {
-  consumer_key: 'zrkDj9IweiTg5exgec8BQg',
-  consumer_secret: '2aAxxcLoanvcu-kM0bxIRXcJMTw',
-  token: 'QWPoFTQfNOQWu-CqP5X8jCOJ7W2njuFq',
-  token_secret: 'DupDQ_5lWQ_gPTUytVG5Gucxus0',
-};
-
-var yelp = new Yelp(yelpConfig);
 
 module.exports = function(ctx, callback) {
+  var yelpConfig = {
+    consumer_key: ctx.data.CONSUMER_KEY,
+    consumer_secret: ctx.data.CONSUMER_SECRET,
+    token: ctx.data.TOKEN,
+    token_secret: ctx.data.TOKEN_SECRET,
+  };
+
+  var yelp = new Yelp(yelpConfig);
   var query = ctx.data.text.split('in');
 
   if (query.length !== 2) {
     return callback(null, {
-        text: INVALID_QUERY
+      text: INVALID_QUERY
     });
   }
 
@@ -30,20 +29,39 @@ module.exports = function(ctx, callback) {
   })
   .then(function(response) {
     var results = response.businesses.map(function(business) {
-        return {
-            text: RESULT_TEMPLATE(business)
-        };
+      return {
+        title: business.name,
+        pretext: business.is_closed ? 'Closed' : 'Open',
+        title_link: business.url,
+        thumb_url: business.image_url,
+        fields: [
+          {
+            title: 'Rating',
+            value: business.rating,
+            short: true
+          },
+          {
+            title: 'Phone',
+            value: business.phone,
+            short: true
+          },
+          {
+            title: 'Location',
+            value: business.location.display_address.join(' ')
+          }
+        ] 
+      };
     });
 
     if (results.length) {
-        callback(null, {
-            text: RESULTS_TEMPLATE({ count: results.length, query: ctx.data.text}),
-            attachments: results
-        });
+      callback(null, {
+        text: RESULTS_TEMPLATE({ count: results.length, query: ctx.data.text}),
+        attachments: results
+      });
     } else {
-        callback(null, {
-            text: NO_RESULT_TEMPLATE({ query: ctx.data.text })
-        });
+      callback(null, {
+        text: NO_RESULT_TEMPLATE({ query: ctx.data.text })
+      });
     }
   })
   .catch(function(response) {
